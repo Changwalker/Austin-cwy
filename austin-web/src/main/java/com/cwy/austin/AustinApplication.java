b784903d4dce6ffe6be1e37fe6ad5b6d6d0a4f6d
Changwalker <511260497@qq.com>
2022-06-16 19:42:08 +0800

    day0

---
+++ austin-web/src/main/java/com/cwy/austin/AustinApplication.java
@@ -0,0 +1,15 @@
+package com.cwy.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cwy
+ */
+
+@SpringBootApplication
+public class AustinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class, args);
+    }
+}
