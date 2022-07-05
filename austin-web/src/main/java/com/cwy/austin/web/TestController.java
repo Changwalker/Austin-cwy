be15f8ad11ddf744da5544557f258edfa1391646
Changwalker <511260497@qq.com>
2022-07-05 14:49:34 +0800

    2022/7/5 5. 引入日志框架（logback.xml->logback-spring.xml，否则失效——加载顺序是先logback.xml然后application.properties）

--- austin-web/src/main/java/com/cwy/austin/web/TestController.java
+++ austin-web/src/main/java/com/cwy/austin/web/TestController.java
@@ -1,12 +1,16 @@
 package com.cwy.austin.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @RequestMapping("/test")
     public String test(){
-        return "GO!";
+        System.out.println("sout:nb");
+        log.info("log:nbnbnb");
+        return "GOGOGO!";
     }
 }
